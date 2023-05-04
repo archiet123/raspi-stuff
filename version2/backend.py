e0efa41c52a2eec0ce3411420fba4764e72826a9
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-05-04 15:10:05 +0100

    add error handling for detecting blank values

--- version2/backend.py
+++ version2/backend.py
@@ -5,6 +5,9 @@ import cv2
 import os
 import argparse
 
+
+
+
 #importing variables from other file
 print(f"\nimage to select from:")
 print("abc123, abcCard, all80, card, first_image, realTest, spacedPunches\n")
@@ -23,6 +26,10 @@ img = cv2.imread(f'ImagesToRead/{getImage}.jpg')#reading init pic
 #cv2.imshow('window', img)
 #cv2.waitKey()
 
+aList = ['A','B','C','D','E','F','G','H','I']
+bList = ['J','K','L','M','N','O','P','Q','R']
+List = []
+
 
 def autoCoords():
 	leftY = 468
@@ -32,6 +39,62 @@ def autoCoords():
 		
 		column0 = img[280:650, leftY:rightY]
 		plt.imsave(f"assets/testing{index}.jpg", column0)		
+
+		
+		leftY +=13
+		rightY +=13
+
+		if index == 0 or index == 72:
+			continue
+
+		if index == 7:
+			#print(index, "removing 4")
+			leftY +=-4
+			rightY +=-4	
+
+		if index % 8 == 0:
+			#print(index, "removing 4")
+			leftY +=-4
+			rightY +=-4				
+
+		if index % 22 == 0:
+			#print(index, "removing 4")
+			leftY +=-4
+			rightY +=-4
+		
+		if index % 30 == 0:
+			#print(index, "removing 4")
+			leftY +=-4
+			rightY +=-4
+
+		if index % 38 == 0:
+			#print(index, "added 4")
+			leftY +=-4
+			rightY +=-4
+
+		if index % 51 == 0:
+			#print(index, "added 4")
+			leftY +=-4
+			rightY +=-4
+
+		if index % 78 == 0:
+			#print(index, "added 4")
+			leftY +=-4
+			rightY +=-4
+
+		if index % 79 == 0:
+			#print(index, "added 4")
+			leftY +=-4
+			rightY +=-4
+
+def topRow():
+	leftY = 468
+	rightY = 480
+	
+	for index in range(0,81):
+		
+		column0 = img[210:280, leftY:rightY]
+		plt.imsave(f"top_row_images/testing{index}.jpg", column0)		
 
 		# if index >= 40: 
 		# 	leftY +=12
@@ -83,7 +146,9 @@ def autoCoords():
 			leftY +=-4
 			rightY +=-4
 
+
 autoCoords()
+topRow()
 
 def getCharacter(final):	
 	selector = 0
@@ -108,3 +173,7 @@ def getCharacter(final):
 	elif final > 20:
 		selector = 0
 	return selector
+
+
+
+	

89951dcc5f18f723fa3ee751333ff8f56dd11195
archiet123 <114408893+archiet123@users.noreply.github.com>
2023-05-22 17:40:47 +0100

    getting values from a 2d array

--- version2/TestingImages/testing.py
+++ version2/TestingImages/testing.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+# from matplotlib import imread
 import cv2
 import os
 import argparse
+import PIL
 
 #links
 #https://pyimagesearch.com/2016/10/31/detecting-multiple-bright-spots-in-an-image-with-python-and-opencv/
@@ -11,40 +13,19 @@ import argparse
 #https://www.programcreek.com/python/example/88833/skimage.measure.label
 
 
-from imutils import contours
-from skimage import measure
-Image = ('testing.jpg') # save images as newimage{column index} 
-read = cv2.imread(Image)#this will need to loop through all images that need to be read
+# from imutils import contours
+# from skimage import measure
+# Image = ('testing.jpg') # save images as newimage{column index} 
+# read = cv2.imread(Image)#this will need to loop through all images that need to be read
+
+image = cv2.imread("eroded.jpg")
+res = np.argwhere((image > 250).any(-1))
+
+#res = np.argwhere(image[:,:,0] > 250)
+# res = np.where(image > 200)
+
+print(str(res))
+print(len(res))
 
 
-#  constants
-BINARY_THRESHOLD = 20
-CONNECTIVITY = 8
-DRAW_CIRCLE_RADIUS = 4
-
-#  convert to gray
-gray_image = cv2.cvtColor(read, cv2.COLOR_BGR2GRAY)
-
-#  extract edges
-binary_image = cv2.Laplacian(gray_image, cv2.CV_8UC1)
-
-#  fill in the holes between edges with dilation
-dilated_image = cv2.dilate(binary_image, np.ones((5, 5)))
-
-#  threshold the black/ non-black areas
-_, thresh = cv2.threshold(dilated_image, BINARY_THRESHOLD, 255, cv2.THRESH_BINARY)
-
-#  find connected components
-components = cv2.connectedComponentsWithStats(thresh, CONNECTIVITY, cv2.CV_32S)
-
-#  draw circles around center of components
-#see connectedComponentsWithStats function for attributes of components variable
-centers = components[3]
-for center in centers:
-    cv2.circle(thresh, (int(center[0]), int(center[1])), DRAW_CIRCLE_RADIUS, (255), thickness=-1)
-
-cv2.imwrite("res.png", thresh)
-locate = cv2.minMaxLoc(thresh)
-print(locate)
-cv2.imshow("res", thresh)
-cv2.waitKey(0)
+# np.argwhere(Image[255,255,255] > threshold)
